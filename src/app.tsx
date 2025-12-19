d7a2cbe902948a65f61b3695e5f90293c01e71ea
pewlax <pewlax@gmail.com>
2025-12-19 22:48:14 +0100

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,27 @@
+import Hero from './components/Hero';
+import VideoSection from './components/VideoSection';
+import Founders from './components/Founders';
+import Benefits from './components/Benefits';
+import NetworkMembers from './components/NetworkMembers';
+import Trips from './components/Trips';
+import HowToApply from './components/HowToApply';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Navigation />
+      <Hero />
+      <VideoSection />
+      <Founders />
+      <Benefits />
+      <NetworkMembers />
+      <Trips />
+      <HowToApply />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
